f0df7e19ab8e4dbab8897369f3c3797d23ab59b0
JonaNorman <kingofzqj@gmail.com>
2023-08-18 16:09:07 +0800

    ToneMappingBT2390

--- sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMappingBT2390.kt
+++ sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMappingBT2390.kt
@@ -3,35 +3,14 @@ package com.norman.android.hdrsample.transform.shader
 import com.norman.android.hdrsample.opengl.GLShaderCode
 
 object ToneMappingBT2390 : GLShaderCode() {
+    // ITU-R BT.2390 EETF
+    // https://www.itu.int/pub/R-REP-BT.2390
     override val code: String
         get() = """
-            //https://github.com/natural-harmonia-gropius/hdr-toys
-            ////https://code.videolan.org/videolan/libplacebo/-/blob/master/src/tone_mapping.c
-            // ITU-R BT.2390 EETF
-            // https://www.itu.int/pub/R-REP-BT.2390
-
-            //!PARAM L_hdr
-            //!TYPE float
-            //!MINIMUM 0
-            //!MAXIMUM 10000
-            1000.0
-
-            //!PARAM L_sdr
-            //!TYPE float
-            //!MINIMUM 0
-            //!MAXIMUM 1000
-            203.0
-
-            //!PARAM CONTRAST_sdr
-            //!TYPE float
-            //!MINIMUM 0
-            //!MAXIMUM 1000000
-            1000.0
-
-            //!HOOK OUTPUT
-            //!BIND HOOKED
-            //!DESC tone mapping (bt.2390)
-
+            #define L_hdr 1000.0
+            #define L_sdr 203.0
+            #define CONTRAST_sdr 1000.0
+            
             const float DISPGAMMA = 2.4;
             const float L_W = 1.0;
             const float L_B = 0.0;
@@ -50,19 +29,14 @@ object ToneMappingBT2390 : GLShaderCode() {
                 return L;
             }
 
-            vec3 tone_mapping_clip(vec3 color) {
-                color.rgb = vec3(
-                    bt1886_r(color.r, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
-                    bt1886_r(color.g, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
-                    bt1886_r(color.b, DISPGAMMA, L_W, L_W / CONTRAST_sdr)
-                );
+            float curve_clip(float x) {
+                x = bt1886_r(x, DISPGAMMA, L_W, L_W / CONTRAST_sdr);
+                x = bt1886_f(x, DISPGAMMA, L_W, L_B);
+                return x;
+            }
 
-                color.rgb = vec3(
-                    bt1886_f(color.r, DISPGAMMA, L_W, L_B),
-                    bt1886_f(color.g, DISPGAMMA, L_W, L_B),
-                    bt1886_f(color.b, DISPGAMMA, L_W, L_B)
-                );
-                return color;
+            vec3 tone_mapping_rgb(vec3 RGB) {
+                return vec3(curve_clip(RGB.r), curve_clip(RGB.g), curve_clip(RGB.b));
             }
 
             const float pq_m1 = 0.1593017578125;
@@ -90,88 +64,91 @@ object ToneMappingBT2390 : GLShaderCode() {
                 return L * pq_C;
             }
 
-            vec3 RGB_to_XYZ(float R, float G, float B) {
+            vec3 RGB_to_XYZ(vec3 RGB) {
                 mat3 M = mat3(
-                    0.6370, 0.1446, 0.1689,
-                    0.2627, 0.6780, 0.0593,
-                    0.0000, 0.0281, 1.0610);
-                return vec3(R, G, B) * M;
+                    0.6369580483012914, 0.14461690358620832,  0.1688809751641721,
+                    0.2627002120112671, 0.6779980715188708,   0.05930171646986196,
+                    0.000000000000000,  0.028072693049087428, 1.060985057710791);
+                return RGB * M;
             }
 
-            vec3 XYZ_to_RGB(float X, float Y, float Z) {
+            vec3 XYZ_to_RGB(vec3 XYZ) {
                 mat3 M = mat3(
-                     1.7167, -0.3557, -0.2534,
-                    -0.6667,  1.6165,  0.0158,
-                     0.0176, -0.0428,  0.9421);
-                return vec3(X, Y, Z) * M;
+                     1.716651187971268,  -0.355670783776392, -0.253366281373660,
+                    -0.666684351832489,   1.616481236634939,  0.0157685458139111,
+                     0.017639857445311,  -0.042770613257809,  0.942103121235474);
+                return XYZ * M;
             }
 
-            vec3 XYZ_to_LMS(float X, float Y, float Z) {
+            vec3 XYZ_to_LMS(vec3 XYZ) {
                 mat3 M = mat3(
                      0.3592, 0.6976, -0.0358,
                     -0.1922, 1.1004,  0.0755,
                      0.0070, 0.0749,  0.8434);
-                return vec3(X, Y, Z) * M;
+                return XYZ * M;
             }
 
-            vec3 LMS_to_XYZ(float L, float M, float S) {
-                mat3 MM = mat3(
+            vec3 LMS_to_XYZ(vec3 LMS) {
+                mat3 M = mat3(
                      2.070180056695613509600, -1.326456876103021025500,  0.206616006847855170810,
                      0.364988250032657479740,  0.680467362852235141020, -0.045421753075853231409,
                     -0.049595542238932107896, -0.049421161186757487412,  1.187995941732803439400);
-                return vec3(L, M, S) * MM;
+                return LMS * M;
             }
 
-            vec3 LMS_to_ICtCp(float L, float M, float S) {
-                vec3 VV = vec3(L, M, S);
-                VV.r = Y_to_ST2084(VV.r);
-                VV.g = Y_to_ST2084(VV.g);
-                VV.b = Y_to_ST2084(VV.b);
-                mat3 MM = mat3(
+            vec3 LMS_to_ICtCp(vec3 LMS) {
+                LMS.x = Y_to_ST2084(LMS.x);
+                LMS.y = Y_to_ST2084(LMS.y);
+                LMS.z = Y_to_ST2084(LMS.z);
+                mat3 M = mat3(
                      2048,   2048,    0,
                      6610, -13613, 7003,
                     17933, -17390, -543) / 4096;
-                return VV * MM;
+                return LMS * M;
             }
 
-            vec3 ICtCp_to_LMS(float I, float Ct, float Cp) {
-                vec3 VV = vec3(I, Ct, Cp);
-                mat3 MM = mat3(
+            vec3 ICtCp_to_LMS(vec3 ICtCp) {
+                mat3 M = mat3(
                     0.99998889656284013833,  0.00860505014728705821,  0.11103437159861647860,
                     1.00001110343715986160, -0.00860505014728705821, -0.11103437159861647860,
                     1.00003206339100541200,  0.56004913547279000113, -0.32063391005412026469);
-                VV *= MM;
-                VV.r = ST2084_to_Y(VV.r);
-                VV.g = ST2084_to_Y(VV.g);
-                VV.b = ST2084_to_Y(VV.b);
-                return VV;
+                ICtCp *= M;
+                ICtCp.x = ST2084_to_Y(ICtCp.x);
+                ICtCp.y = ST2084_to_Y(ICtCp.y);
+                ICtCp.z = ST2084_to_Y(ICtCp.z);
+                return ICtCp;
             }
 
-            vec3 RGB_to_Ictcp(vec3 color, float L_sdr) {
+            vec3 RGB_to_ICtCp(vec3 color) {
                 color *= L_sdr;
-                color = RGB_to_XYZ(color.r, color.g, color.b);
-                color = XYZ_to_LMS(color.r, color.g, color.b);
-                color = LMS_to_ICtCp(color.r, color.g, color.b);
+                color = RGB_to_XYZ(color);
+                color = XYZ_to_LMS(color);
+                color = LMS_to_ICtCp(color);
                 return color;
             }
 
-            vec3 Ictcp_to_RGB(vec3 color, float L_sdr) {
-                color = ICtCp_to_LMS(color.r, color.g, color.b);
-                color = LMS_to_XYZ(color.r, color.g, color.b);
-                color = XYZ_to_RGB(color.r, color.g, color.b);
+            vec3 ICtCp_to_RGB(vec3 color) {
+                color = ICtCp_to_LMS(color);
+                color = LMS_to_XYZ(color);
+                color = XYZ_to_RGB(color);
                 color /= L_sdr;
                 return color;
             }
 
-            float f(float x, float L_w, float L_b, float L_max, float L_min) {
-                const float maxLum = (L_max - L_b) / (L_w - L_b);
-                const float minLum = (L_min - L_b) / (L_w - L_b);
+            float curve(float x) {
+                const float iw = Y_to_ST2084(L_hdr);
+                const float ib = Y_to_ST2084(0.0);
+                const float ow = Y_to_ST2084(L_sdr);
+                const float ob = Y_to_ST2084(L_sdr / CONTRAST_sdr);
+
+                const float maxLum = (ow - ib) / (iw - ib);
+                const float minLum = (ob - ib) / (iw - ib);
 
                 const float KS = 1.5 * maxLum - 0.5;
                 const float b = minLum;
 
                 // E1
-                x = (x - L_b) / (L_w - L_b);
+                x = (x - ib) / (iw - ib);
 
                 // E2
                 if (KS <= x) {
@@ -192,30 +169,24 @@ object ToneMappingBT2390 : GLShaderCode() {
                 }
 
                 // E4
-                x = x * (L_w - L_b) + L_b;
+                x = x * (iw - ib) + ib;
 
                 return x;
             }
 
-            vec3 tone_mapping(vec3 Ictcp) {
-                const float iw = Y_to_ST2084(L_hdr);
-                const float ib = Y_to_ST2084(0.0);
-                const float ow = Y_to_ST2084(L_sdr);
-                const float ob = Y_to_ST2084(L_sdr / CONTRAST_sdr);
+            vec3 tone_mapping_ictcp(vec3 ICtCp) {
+                float I2  = curve(ICtCp.x);
+                ICtCp.yz *= min(ICtCp.x / I2, I2 / ICtCp.x);
+                ICtCp.x   = I2;
 
-                float I2  = f(Ictcp.x, iw, ib, ow, ob);
-                Ictcp.yz *= min(Ictcp.x / I2, I2 / Ictcp.x);
-                Ictcp.x   = I2;
-
-                return Ictcp;
+                return ICtCp;
             }
 
-            vec4 color = HOOKED_tex(HOOKED_pos);
-            vec4 hook() {
-                color.rgb = RGB_to_Ictcp(color.rgb, L_sdr);
-                color.rgb = tone_mapping(color.rgb);
-                color.rgb = Ictcp_to_RGB(color.rgb, L_sdr);
-                color.rgb = tone_mapping_clip(color.rgb);
+            vec4 ${javaClass.name}(vec4 color) {
+                color.rgb = RGB_to_ICtCp(color.rgb);
+                color.rgb = tone_mapping_ictcp(color.rgb);
+                color.rgb = ICtCp_to_RGB(color.rgb);
+                color.rgb = tone_mapping_rgb(color.rgb);
                 return color;
             }
         """.trimIndent()
