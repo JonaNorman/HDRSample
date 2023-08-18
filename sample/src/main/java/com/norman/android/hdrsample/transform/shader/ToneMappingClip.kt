9c2087ef716061bb4da9ca99b1dd4be9da78a34e
JonaNorman <kingofzqj@gmail.com>
2023-08-18 16:04:10 +0800

    ToneMappingClip

--- sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMappingClip.kt
+++ sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMappingClip.kt
@@ -3,20 +3,10 @@ package com.norman.android.hdrsample.transform.shader
 import com.norman.android.hdrsample.opengl.GLShaderCode
 
 object ToneMappingClip : GLShaderCode() {
+    // Crop code values that are out of the SDR range
     override val code: String
         get() = """
-            // Crop code values that are out of the SDR range
-
-            //!PARAM CONTRAST_sdr
-            //!TYPE float
-            //!MINIMUM 0
-            //!MAXIMUM 1000000
-            1000.0
-
-            //!HOOK OUTPUT
-            //!BIND HOOKED
-            //!DESC tone mapping (clip)
-
+            #define CONTRAST_sdr 1000.0
             const float DISPGAMMA = 2.4;
             const float L_W = 1.0;
             const float L_B = 0.0;
@@ -35,24 +25,19 @@ object ToneMappingClip : GLShaderCode() {
                 return L;
             }
 
-            vec3 tone_mapping_clip(vec3 color) {
-                color.rgb = vec3(
-                    bt1886_r(color.r, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
-                    bt1886_r(color.g, DISPGAMMA, L_W, L_W / CONTRAST_sdr),
-                    bt1886_r(color.b, DISPGAMMA, L_W, L_W / CONTRAST_sdr)
-                );
-
-                color.rgb = vec3(
-                    bt1886_f(color.r, DISPGAMMA, L_W, L_B),
-                    bt1886_f(color.g, DISPGAMMA, L_W, L_B),
-                    bt1886_f(color.b, DISPGAMMA, L_W, L_B)
-                );
-                return color;
+            float curve(float x) {
+                x = bt1886_r(x, DISPGAMMA, L_W, L_W / CONTRAST_sdr);
+                x = bt1886_f(x, DISPGAMMA, L_W, L_B);
+                return x;
             }
 
-            vec4 color = HOOKED_tex(HOOKED_pos);
-            vec4 hook() {
-                color.rgb = tone_mapping_clip(color.rgb);
+            vec3 tone_mapping_rgb(vec3 RGB) {
+                return vec3(curve(RGB.r), curve(RGB.g), curve(RGB.b));
+            }
+
+            vec4 ${javaClass.name}(vec4 color) {
+                color.rgb = tone_mapping_rgb(color.rgb);
+
                 return color;
             }
         """.trimIndent()
