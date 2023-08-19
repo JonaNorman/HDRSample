e178a5eae4008dab70f653ceff552683ee0cea75
JonaNorman <kingofzqj@gmail.com>
2023-08-19 20:25:27 +0800

    色域映射直接裁剪

--- sample/src/main/java/com/norman/android/hdrsample/transform/shader/GamutMapClip.kt
+++ sample/src/main/java/com/norman/android/hdrsample/transform/shader/GamutMapClip.kt
@@ -1,18 +1,22 @@
 package com.norman.android.hdrsample.transform.shader
 
 import com.norman.android.hdrsample.opengl.GLShaderCode
+import com.norman.android.hdrsample.transform.shader.ColorSpaceConversion.methodBt2020ToBt709
 
+/**
+ * 直接裁剪BT2020转BT709色域映射后超出范围的值
+ */
 object GamutMapClip: GLShaderCode() {
-    /**
-     * 直接裁剪BT2020转BT709后超出范围的值
-     */
+
+    init {
+        includeList.add(ColorSpaceConversion)
+    }
     override val code: String
         get() = """
         vec4 ${javaClass.name}(vec4 color) {
-            vec3 color_src = color.rgb;
-            vec3 color_dst = BT2020_TO_BT709(color_src);
-            vec3 color_dst_cliped = clamp(color_dst, 0.0, 1.0);
-            color.rgb = color_dst_cliped;
+            vec3 rgb = color.rgb;
+            rgb = $methodBt2020ToBt709(rgb);
+            color.rgb = clamp(rgb, 0.0, 1.0);
             return color;
         }
         """.trimIndent()
