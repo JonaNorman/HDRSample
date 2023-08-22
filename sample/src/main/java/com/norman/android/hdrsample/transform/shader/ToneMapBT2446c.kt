cc2b37ffe5039505faa58657e9f0c4957dce1a52
JonaNorman <kingofzqj@gmail.com>
2023-08-22 17:46:41 +0800

    ToneMapBT2446c

--- sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMapBT2446c.kt
+++ sample/src/main/java/com/norman/android/hdrsample/transform/shader/ToneMapBT2446c.kt
@@ -3,54 +3,13 @@ package com.norman.android.hdrsample.transform.shader
 import com.norman.android.hdrsample.opengl.GLShaderCode
 
 //https://github.com/natural-harmonia-gropius/hdr-toys/blob/master/tone-mapping/bt2446c.glsl
-object ToneMapBT2446c : GLShaderCode() {
+object ToneMapBT2446c : GamutMap() {
     // ITU-R BT.2446 Conversion Method C
     // https://www.itu.int/pub/R-REP-BT.2446
     override val code: String
         get() = """
-          vec3 RGB_to_XYZ(vec3 RGB) {
-              mat3 M = mat3(
-                  0.6369580483012914, 0.14461690358620832,  0.1688809751641721,
-                  0.2627002120112671, 0.6779980715188708,   0.05930171646986196,
-                  0.000000000000000,  0.028072693049087428, 1.060985057710791);
-              return RGB * M;
-          }
-
-          vec3 XYZ_to_RGB(vec3 XYZ) {
-              mat3 M = mat3(
-                   1.716651187971268,  -0.355670783776392, -0.253366281373660,
-                  -0.666684351832489,   1.616481236634939,  0.0157685458139111,
-                   0.017639857445311,  -0.042770613257809,  0.942103121235474);
-              return XYZ * M;
-          }
-
-          vec3 XYZ_to_xyY(vec3 XYZ) {
-              float X = XYZ.x;
-              float Y = XYZ.y;
-              float Z = XYZ.z;
-
-              float divisor = X + Y + Z;
-              if (divisor == 0.0) divisor = 1e-6;
-
-              float x = X / divisor;
-              float y = Y / divisor;
-
-              return vec3(x, y, Y);
-          }
-
-          vec3 xyY_to_XYZ(vec3 xyY) {
-              float x = xyY.x;
-              float y = xyY.y;
-              float Y = xyY.z;
-
-              float multiplo = Y / max(y, 1e-6);
-
-              float z = 1.0 - x - y;
-              float X = x * multiplo;
-              float Z = z * multiplo;
-
-              return vec3(X, Y, Z);
-          }
+         
+          
 
           const float ip = 0.58535;   // linear length
           const float k1 = 0.83802;   // linear strength
@@ -70,12 +29,12 @@ object ToneMapBT2446c : GLShaderCode() {
               return f(x, k1, k3, ip) / over_white;
           }
 
-          vec4 ${javaClass.name}(vec4 color) {
-              color.rgb = RGB_to_XYZ(color.rgb);
-              color.rgb = XYZ_to_xyY(color.rgb);
+          vec3 $methodGamutMap(vec3 color) {
+              color.rgb = ${ColorSpaceConversion.methodBt2020ToXYZ}(color.rgb);
+              color.rgb = ${ColorSpaceConversion.methodXYZToxyY}(color.rgb);
               color.z   = curve(color.z);
-              color.rgb = xyY_to_XYZ(color.rgb);
-              color.rgb = XYZ_to_RGB(color.rgb);
+              color.rgb = ${ColorSpaceConversion.methodxyYToXYZ}(color.rgb);
+              color.rgb = ${ColorSpaceConversion.methodXYZToBt2020}(color.rgb);
               return color;
           }
         """.trimIndent()
